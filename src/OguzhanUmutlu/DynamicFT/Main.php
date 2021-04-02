1c296402ed15079d5b9ac73093c20933aa8bc03e
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-04-02 22:20:18 +0300

    v1.0.0

--- src/OguzhanUmutlu/DynamicFT/Main.php
+++ src/OguzhanUmutlu/DynamicFT/Main.php
@@ -2,6 +2,9 @@
 
 namespace OguzhanUmutlu\DynamicFT;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\level\particle\FloatingTextParticle;
@@ -16,6 +19,7 @@ class Main extends PluginBase implements Listener {
     public $ftConfig;
     public $fts = [];
     public $config;
+    public $commands = [];
     static $instance;
 
     public function onEnable(): void {
@@ -25,12 +29,134 @@ class Main extends PluginBase implements Listener {
         $this->ftConfig = new Config($this->getDataFolder() . "fts.yml", Config::YAML, ["data" => []]);
         $this->getScheduler()->scheduleRepeatingTask(new FtTask($this), intval((float)$this->config->getNested("checkSeconds") * 20));
         foreach($this->ftConfig->getNested("data") as $ft) {
-            var_dump($this->registerFT($ft["text"], new Position($ft["x"], $ft["y"], $ft["z"], $this->getServer()->getLevelByName($ft["level"])), false));
+            $this->registerFT($ft["text"], new Position($ft["x"], $ft["y"], $ft["z"], $this->getServer()->getLevelByName($ft["level"])), false);
         }
     }
 
     static function getInstance() {
         return self::$instance;
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        $args[0] = strtolower($args[0]);
+        if(!isset($this->commands[$sender->getName()])) {
+            $this->commands[$sender->getName()] = [];
+        }
+        switch($args[0]) {
+            case "spawn":
+            case "create":
+            case "s":
+            case "c":
+                if(!$sender->hasPermission($command->getPermission().".create")) {
+                    $sender->sendMessage("§c> You don't have permission to create floating texts.");
+                    return true;
+                }
+                $sender->sendMessage("§e> Type the text of the floating text. To cancel type \$cancel to chat.");
+                $this->commands[$sender->getName()]["create"] = true;
+                break;
+            case "edit":
+            case "e":
+                if(!$sender->hasPermission($command->getPermission().".edit")) {
+                    $sender->sendMessage("§c> You don't have permission to edit floating texts.");
+                    return true;
+                }
+                switch($args[1]) {
+                    case "tphere":
+                    case "tpme":
+                        if(!$sender->hasPermission($command->getPermission().".edit.tphere")) {
+                            $sender->sendMessage("§c> You don't have permission to edit floating texts' position.");
+                            return true;
+                        }
+                        if(!isset($args[2])) {
+                            $sender->sendMessage("§c> Usage: /dft edit tpme [ id ]");
+                            return true;
+                        }
+                        if(!$this->getRegisteredFt(intval($args[2]))) {
+                            $sender->sendMessage("§c> Floating text not found.");
+                            return true;
+                        }
+                        $this->updateRegisteredFt(intval($args[2]), "x", intval($sender->getX()));
+                        $this->updateRegisteredFt(intval($args[2]), "y", intval($sender->getY()));
+                        $this->updateRegisteredFt(intval($args[2]), "z", intval($sender->getZ()));
+                        $sender->sendMessage("§a> Teleported floating text to you.");
+                        break;
+                    case "tpto":
+                        if(!$sender->hasPermission($command->getPermission().".edit.tpto")) {
+                            $sender->sendMessage("§c> You don't have permission to teleporting floating texts.");
+                            return true;
+                        }
+                        if(!isset($args[2])) {
+                            $sender->sendMessage("§c> Usage: /dft edit tpto [ id ]");
+                            return true;
+                        }
+                        if(!$this->getRegisteredFt(intval($args[2]))) {
+                            $sender->sendMessage("§c> Floating text not found.");
+                            return true;
+                        }
+                        $sender->teleport(new Position($this->getRegisteredFt(intval($args[2]))["x"], $this->getRegisteredFt(intval($args[2]))["y"], $this->getRegisteredFt(intval($args[2]))["z"], $this->getServer()->getLevelByName($this->getRegisteredFt(intval($args[2]))["level"])));
+                        $sender->sendMessage("§a> Teleported you to floating text.");
+                        break;
+                    case "text":
+                        if(!$sender->hasPermission($command->getPermission().".edit.text")) {
+                            $sender->sendMessage("§c> You don't have permission to teleporting floating texts.");
+                            return true;
+                        }
+                        if(!isset($args[2])) {
+                            $sender->sendMessage("§c> Usage: /dft edit text [ id ]");
+                            return true;
+                        }
+                        if(!$this->getRegisteredFt(intval($args[2]))) {
+                            $sender->sendMessage("§c> Floating text not found.");
+                            return true;
+                        }
+                        $sender->sendMessage("§e> Type new text of floating text to chat. To cancel type \$cancel to chat.");
+                        $this->commands[$sender->getName()]["editText"] = $this->getRegisteredFt(intval($args[2]));
+                        break;
+                    default:
+                        $sender->sendMessage("§c> Usage: /dft edit [ tphere, tpto, text ]");
+                        break;
+                }
+                break;
+            case "remove":
+            case "delete":
+            case "r":
+            case "d":
+                if (!$sender->hasPermission($command->getPermission() . ".remove")) {
+                    $sender->sendMessage("§c> You don't have permission to remove floating texts.");
+                    return true;
+                }
+                if (!isset($args[1])) {
+                    $sender->sendMessage("§c> Usage: /dft remove [ id ]");
+                    return true;
+                }
+                if (!$this->getRegisteredFt(intval($args[2]))) {
+                    $sender->sendMessage("§c> Floating text not found.");
+                    return true;
+                }
+                $this->unregisterFT(intval($args[2]));
+                $sender->sendMessage("§a> Floating text removed.");
+                break;
+            case "listids":
+                $list = array_chunk($this->fts, 5);
+                if(!isset($args[1])) {
+                    $args[1] = "1";
+                }
+                if(!is_numeric($args[1])) {
+                    $sender->sendMessage("§c> Usage: /dft listids [ page (1/".count($list)."): number ]");
+                    return true;
+                }
+                $sender->sendMessage("§e> Floating texts, Page ".$args[1]."/".count($list));
+                foreach($list[intval($args[1])-1] as $item) {
+                    $sender->sendMessage("§a> ID: " . $item["id"] . ", Text: " . $item["text"]);
+                }
+                $sender->sendMessage("§e> Floating texts, Page ".$args[1]."/".count($list));
+                break;
+            default:
+                $sender->sendMessage("§c> Usage: /dft [ create, edit, remove, listids ]");
+                break;
+        }
+        return true;
     }
 
     public function onJoin(PlayerJoinEvent $event) {
@@ -48,7 +174,32 @@ class Main extends PluginBase implements Listener {
         }
     }
 
-    public function registerFT(string $text, Position $pos, bool $addToData = false): int {
+    public function onChat(PlayerChatEvent $event) {
+        $player = $event->getPlayer();
+        $message = $event->getMessage();
+        if(!isset($this->commands[$player->getName()])) {
+            return;
+        }
+        if(isset($this->commands[$player->getName()]["editText"]) && $this->commands[$player->getName()]["editText"]) {
+            if($message == "\$cancel") {
+                $this->commands[$player->getName()]["editText"] = false;
+                $player->sendMessage("§a> Action cancelled.");
+                return;
+            }
+            $this->updateRegisteredFt($this->commands[$player->getName()]["editText"]["id"], "text", $message);
+            $player->sendMessage("§a> Floating text's text updated.");
+        } else if(isset($this->commands[$player->getName()]["create"]) && $this->commands[$player->getName()]["create"]) {
+            if($message == "\$cancel") {
+                $this->commands[$player->getName()]["create"] = false;
+                $player->sendMessage("§a> Action cancelled.");
+                return;
+            }
+            $this->registerFT($message, $player->getPosition());
+            $player->sendMessage("§a> Floating text created.");
+        }
+    }
+
+    public function registerFT(string $text, Position $pos, bool $addToData = true): int {
         if(!$this->getServer()->isLevelGenerated($pos->getLevel()->getName())) {
             return -1;
         }
@@ -61,6 +212,9 @@ class Main extends PluginBase implements Listener {
         $ft = ["text" => $text, "x" => $pos->getX(), "y" => $pos->getY(), "z" => $pos->getZ(), "level" => $pos->getLevel()->getFolderName()];
         $a = $this->ftConfig->getNested("data");
         array_push($a, $ft);
+        foreach($this->getServer()->getOnlinePlayers() as $p) {
+            $this->spawnFT($ft["id"], $p);
+        }
         if($addToData) {
             $this->ftConfig->setNested("data", $a);
             $this->ftConfig->save();
@@ -94,6 +248,10 @@ class Main extends PluginBase implements Listener {
         $ft = $this->getRegisteredFt($typeId);
         $ft[$data] = $property;
         $this->registerFT($ft["text"], new Position($ft["x"], $ft["y"], $ft["z"], $this->getServer()->getLevelByName($ft["level"])));
+        foreach($this->ftEntities as $x) {
+            $this->removeFT($x["id"]);
+            $this->spawnFT($x["creationId"], $x["player"]);
+        }
     }
 
     public function getRegisteredFt(int $typeId): ?array {
