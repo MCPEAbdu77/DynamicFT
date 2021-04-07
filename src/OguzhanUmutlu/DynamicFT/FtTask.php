3b3b62db9cb688607db20dfe8bb81d58b5f0bac6
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-04-07 11:37:14 +0300

    v1.0.0

--- src/OguzhanUmutlu/DynamicFT/FtTask.php
+++ src/OguzhanUmutlu/DynamicFT/FtTask.php
@@ -17,90 +17,86 @@ class FtTask extends Task {
 
     public function onRun(int $currentTick) {
         $p = $this->plugin;
-        var_dump("res");
-        var_dump(count($this->plugin->ftEntities));
-        $a = 0;
         foreach($this->plugin->ftEntities as $ft) {
-            $a++;
-            var_dump($a);
-            if(!$ft) break;
-            if(!$ft["player"]->isOnline()) break;
-
-            $particle = $ft["particle"];
-            $player = $ft["player"];
-            $text = $p->getRegisteredFt($ft["creationId"])["text"];
-            $text = str_replace(
-                [
-                    "{player.name}",
-                    "{player.display_name}",
-                    "{server.online}",
-                    "{server.max_online}",
-                    "{player.item.name}",
-                    "{player.item.id}",
-                    "{player.item.meta}",
-                    "{player.item.count}",
-                    "{player.x}",
-                    "{player.y}",
-                    "{player.z}",
-                    "{player.level.name}",
-                    "{player.world.name}",
-                    "{player.level.folder_name}",
-                    "{player.world.folder_name}",
-                    "{player.level.player_count}",
-                    "{player.world.player_count}",
-                    "{player.ip}",
-                    "{player.ping}",
-                    "{time}",
-                    "{date}",
-                    "{line}"
-                ],
-                [
-                    $player->getName(),
-                    $player->getDisplayName(),
-                    count($p->getServer()->getOnlinePlayers()),
-                    $p->getServer()->getMaxPlayers(),
-                    $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getName() : "",
-                    $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getId() : 0,
-                    $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getDamage() : 0,
-                    $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getCount() : 0,
-                    $player->getFloorX(),
-                    $player->getFloorY(),
-                    $player->getFloorZ(),
-                    $player->getLevel()->getName(),
-                    $player->getLevel()->getName(),
-                    $player->getLevel()->getFolderName(),
-                    $player->getLevel()->getFolderName(),
-                    count($player->getLevel()->getPlayers()),
-                    count($player->getLevel()->getPlayers()),
-                    $player->getAddress(),
-                    $player->getPing(),
-                    date("h:i:s A"),
-                    date("j/n/Y"),
-                    "\n"
-                ],
-                $text
-            );
-            if($p->config->getNested("modules.EconomyAPI") && class_exists(EconomyAPI::class)) {
-                $text = str_replace("{player.money}", EconomyAPI::getInstance()->myMoney($player), $text);
+            if($ft && $ft["player"]->isOnline()) {
+                $particle = $ft["particle"];
+                $player = $ft["player"];
+                $text = $p->getRegisteredFt($ft["creationId"])["text"];
+                $text = str_replace(
+                    [
+                        "{player.name}",
+                        "{player.display_name}",
+                        "{server.online}",
+                        "{server.max_online}",
+                        "{player.item.name}",
+                        "{player.item.id}",
+                        "{player.item.meta}",
+                        "{player.item.count}",
+                        "{player.x}",
+                        "{player.y}",
+                        "{player.z}",
+                        "{player.level.name}",
+                        "{player.world.name}",
+                        "{player.level.folder_name}",
+                        "{player.world.folder_name}",
+                        "{player.level.player_count}",
+                        "{player.world.player_count}",
+                        "{player.ip}",
+                        "{player.ping}",
+                        "{time}",
+                        "{date}",
+                        "{line}"
+                    ],
+                    [
+                        $player->getName(),
+                        $player->getDisplayName(),
+                        count($p->getServer()->getOnlinePlayers()),
+                        $p->getServer()->getMaxPlayers(),
+                        $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getName() : "",
+                        $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getId() : 0,
+                        $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getDamage() : 0,
+                        $player->getInventory()->getItemInHand() ? $player->getInventory()->getItemInHand()->getCount() : 0,
+                        $player->getFloorX(),
+                        $player->getFloorY(),
+                        $player->getFloorZ(),
+                        $player->getLevel()->getName(),
+                        $player->getLevel()->getName(),
+                        $player->getLevel()->getFolderName(),
+                        $player->getLevel()->getFolderName(),
+                        count($player->getLevel()->getPlayers()),
+                        count($player->getLevel()->getPlayers()),
+                        $player->getAddress(),
+                        $player->getPing(),
+                        date("h:i:s A"),
+                        date("j/n/Y"),
+                        "\n"
+                    ],
+                    $text
+                );
+                if($p->config->getNested("modules.EconomyAPI") && class_exists(EconomyAPI::class)) {
+                    $text = str_replace("{player.money}", EconomyAPI::getInstance()->myMoney($player), $text);
+                }
+                if($p->config->getNested("modules.FactionsPro") && class_exists(FactionMain::class) && $p->getServer()->getPluginManager()->getPlugin("FactionsPro")) {
+                    $text = str_replace(["{player.faction.name}", "{player.faction.power}"], [
+                        $p->getServer()->getPluginManager()->getPlugin("FactionsPro")->getPlayerFaction($player),
+                        $p->getServer()->getPluginManager()->getPlugin("FactionsPro")->getFactionPower($player)
+                    ], $text);
+                }
+                if($particle->getTitle() != $text) {
+                    $particle->setTitle($text);
+                    $ftt = $p->getRegisteredFt($ft["creationId"]);
+                    $pos = new Position($ftt["x"], $ftt["y"], $ftt["z"], $p->getServer()->getLevelByName($ftt["level"]));
+                    if($p->getServer()->isLevelGenerated($ftt["level"])) {
+                        if(!$p->getServer()->isLevelLoaded($ftt["level"])) {
+                            $p->getServer()->loadLevel($ftt["level"]);
+                        }
+                        if(!$pos->getLevel()->isChunkLoaded($pos->getX() >> 4, $pos->getZ() >> 4)) {
+                            $pos->getLevel()->loadChunk($pos->getX() >> 4, $pos->getZ() >> 4);
+                        }
+                        $p->updateFt($ft["id"], "particle", $particle);
+                    }
+                }
             }
-            if($p->config->getNested("modules.FactionsPro") && class_exists(FactionMain::class) && $p->getServer()->getPluginManager()->getPlugin("FactionsPro")) {
-                $text = str_replace(["{player.faction.name}", "{player.faction.power}"], [
-                    $p->getServer()->getPluginManager()->getPlugin("FactionsPro")->getPlayerFaction($player),
-                    $p->getServer()->getPluginManager()->getPlugin("FactionsPro")->getFactionPower($player)
-                ], $text);
-            }
-            if($particle->getTitle() == $text) break;
-            $particle->setTitle($text);
-            $ftt = $p->getRegisteredFt($ft["creationId"]);
-            $pos = new Position($ftt["x"], $ftt["y"], $ftt["z"], $p->getServer()->getLevelByName($ftt["level"]));
-            if(!$p->getServer()->isLevelGenerated($ftt["level"])) break;
-            if(!$p->getServer()->isLevelLoaded($ftt["level"])) {
-                $p->getServer()->loadLevel($ftt["level"]);
-            }
-            if(!$pos->getLevel()->isChunkLoaded($pos->getX() >> 4, $pos->getZ() >> 4)) {
-                $pos->getLevel()->loadChunk($pos->getX() >> 4, $pos->getZ() >> 4);
-            }
-            $p->updateFt($ft["id"], "particle", $particle);
         }
     }
 }
